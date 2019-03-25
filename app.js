a4b07622a4410a331f1c1e1d3a5f99abbdf86875
ningdong <344805262@qq.com>
2019-03-25 22:35:58 +0800

    添加index页面

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+/**
+ * 公共逻辑脚本
+ */
 App({
+  // 全局数据todos
   todos: [
     { text: 'Learning Javascript', completed: true },
     { text: 'Learning ES2016', completed: true },
@@ -7,10 +11,12 @@ App({
 
   userInfo: null,
 
+  // 全局方法
   getUserInfo() {
     return new Promise((resolve, reject) => {
       if (this.userInfo) resolve(this.userInfo);
 
+      // 调用用户授权api获取用户信息
       my.getAuthCode({
         scopes: ['auth_user'],
         success: authcode => {
