0d4eac45f70e010b6b789766a2f998e415b84120
ningdong <344805262@qq.com>
2020-04-30 18:59:00 +0800

    gulp

--- parttimeApp/gulpfile.js
+++ parttimeApp/gulpfile.js
@@ -6,8 +6,9 @@ const path = require('path');
 
 const DEST = './parttimeApp';
 
+/*--------------------分隔线---------------------*/
 
-gulp.task('css', function () {
+exports.default = function () {
     const processors = [pxtoviewport({
         viewportWidth: 1920,
         viewportHeight: 562,
@@ -24,26 +25,7 @@ gulp.task('css', function () {
         landscapeUnit: 'vw',
         landscapeWidth: 568
     })]
-
     return gulp.src(['stylesheets/build/*.css'])
         .pipe(postcss(processors))
         .pipe(gulp.dest(path.join('stylesheets/dist')))
-})
-
-/*--------------------分隔线---------------------*/
-const {src, dest} = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-
-exports.default = function () {
-    return src('src/script/*.js')
-        .pipe(dest('src/script/output/'));
-}
-
-exports.default = function() {
-    return src('src/script/*.js')
-        .pipe(babel())
-        .pipe(src('src/script/vendor/*.js'))
-        .pipe(uglify())
-        .pipe(dest('src/script/output/'));
 }
